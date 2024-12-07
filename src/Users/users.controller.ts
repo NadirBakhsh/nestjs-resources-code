25866075d3e7109d850b677e3fff674aa5b2ef13
NadirBakhsh <nadir4k2010@gmail.com>
2024-12-07 13:28:01 +0500

    Working With Routing Decorators

--- src/Users/users.controller.ts
+++ src/Users/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Patch } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  public getUsers() {
+    return 'Get all users';
+  }
+
+  @Post()
+  public createUser() {
+    return 'Create user';
+  }
+
+  @Put()
+  public updateUser() {
+    return 'Update user';
+  }
+
+  @Delete()
+  public removeUser() {
+    return 'Remove user';
+  }
+
+  @Patch()
+  public updatePartialUser() {
+    return 'Update partial user';
+  }
+}
