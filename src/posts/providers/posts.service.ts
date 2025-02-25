db4b1e404267fd52479db8179179fe78a0b79743
NadirBakhsh <nadir4k2010@gmail.com>
2025-02-25 18:11:18 +0500

    Return Posts From Posts Service

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { title } from 'process';
 
 @Injectable()
 export class PostsService {
     public findAll(userId: string) {
       console.log('userId:', userId);
+      // Users Service
+      // Find A User
+      return [
+        {
+          title: 'Post 1',
+          content: 'Content 1'
+        },
+        {
+          title: 'Post 2',
+          content: 'Content 2'
+        }
+      ];
+
     }
 }
 
