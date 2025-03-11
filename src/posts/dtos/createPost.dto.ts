cf24b05bc142e60c99f1b9ed9a8a6ff405f248e5
NadirBakhsh <nadir4k2010@gmail.com>
2025-03-11 12:58:51 +0500

    Adding Validations to CreatePostDto

--- src/posts/dtos/createPost.dto.ts
+++ src/posts/dtos/createPost.dto.ts
@@ -1,15 +1,59 @@
+import {
+  IsArray,
+  IsEnum,
+  IsISO8601,
+  IsJSON,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Matches,
+  MinLength,
+} from 'class-validator';
 import { postStatus } from '../enums/postStatus.enum';
 import { postType } from './../enums/postType.enum';
 export class CreatePostDto {
-    title: string;
-    postType: postType;
-    slug: string;
-    status: postStatus;
-    content?: string;
-    schema?: string;
-    featuredImageUrl?: string;
-    publishOn?: Date;
-    tags: string[];
-    metaOptions: [{key: 'sidebarEnabled', value: true}];
+  @IsString()
+  @MinLength(3)
+  @IsNotEmpty()
+  title: string;
 
+  @IsEnum(postType)
+  @IsNotEmpty()
+  postType: postType;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[A-Za-z0-9]+(?:-[A-Za-z0-9]+)*$/, {
+    message: 'Slug must be a valid slug',
+  })
+  slug: string;
+
+  @IsEnum(postStatus)
+  @IsNotEmpty()
+  status: postStatus;
+
+  @IsString()
+  @IsOptional()
+  content?: string;
+
+  @IsOptional()
+  @IsJSON()
+  schema?: string;
+
+  @IsOptional()
+  @IsUrl()
+  featuredImageUrl?: string;
+
+  @IsISO8601()
+  @IsOptional()
+  publishOn?: Date;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  @MinLength(3, { each: true })
+  tags: string[];
+
+  metaOptions: [{ key: 'sidebarEnabled'; value: true }];
 }
