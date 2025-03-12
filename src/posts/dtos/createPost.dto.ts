cce95c29af500d09eeb3fd4e25896aca44167d82
NadirBakhsh <nadir4k2010@gmail.com>
2025-03-12 11:55:32 +0500

    Using Swagger For Documenting CreatePostDto

--- src/posts/dtos/createPost.dto.ts
+++ src/posts/dtos/createPost.dto.ts
@@ -15,16 +15,30 @@ import { postStatus } from '../enums/postStatus.enum';
 import { postType } from './../enums/postType.enum';
 import { CreatePostMetaOptionsDto } from './createPostMetaOptions.dto';
 import { Type } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 export class CreatePostDto {
+  @ApiProperty({
+    description: 'This is the title for the blog post',
+    example: 'This is a title',
+  })
   @IsString()
   @MinLength(3)
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    description: 'Please select the type of post',
+    enum: postType,
+    example: 'draft',
+  })
   @IsEnum(postType)
   @IsNotEmpty()
   postType: postType;
 
+  @ApiProperty({
+    description: 'Here it is the slug for the blog post',
+    example: 'post-url-slug',
+  })
   @IsString()
   @IsNotEmpty()
   @Matches(/^[A-Za-z0-9]+(?:-[A-Za-z0-9]+)*$/, {
@@ -32,37 +46,82 @@ export class CreatePostDto {
   })
   slug: string;
 
+  @ApiProperty({
+    description: 'Select the status of the post',
+    enum: postStatus,
+    example: 'published',
+  })
   @IsEnum(postStatus)
   @IsNotEmpty()
   status: postStatus;
 
+  @ApiPropertyOptional({
+    description: 'This is the content of the post',
+    example: 'The post content',
+  })
   @IsString()
   @IsOptional()
   content?: string;
 
+  @ApiPropertyOptional({
+    description:
+      'Serialize your JSON object else a validation error will be throw',
+    example:
+      '{\r\n  "@context": "https:\\/\\/schema.org",\r\n  "@type": "Person"\r\n}',
+  })
   @IsOptional()
   @IsJSON()
   schema?: string;
 
+  @ApiPropertyOptional({
+    description: 'featured image for your blog post',
+    example: 'https://example.com/image.jpg',
+  })
   @IsOptional()
   @IsUrl()
   featuredImageUrl?: string;
 
+  @ApiPropertyOptional({
+    description: 'Publish date',
+    example: '2020-01-01T00:00:00.000Z',
+  })
   @IsISO8601()
   @IsOptional()
   publishOn?: Date;
-  
+
+  @ApiPropertyOptional({
+    description: 'Array of tags passed as string',
+    example: ['tag1', 'tag2'],
+  })
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
   @MinLength(3, { each: true })
   tags: string[];
-  
+
+  @ApiPropertyOptional({
+    type: 'array',
+    required: false,
+    items: {
+      type: 'object',
+      properties: {
+        key: {
+          type: 'string',
+          description:
+            'the key can be any string indentifer for your meta option',
+          example: 'sidebarEnabled',
+        },
+        value: {
+          type: 'any',
+          description: 'Any value you want to save to the key',
+          example: true,
+        },
+      },
+    },
+  })
   @IsOptional()
   @IsArray()
-  @ValidateNested({each: true})
+  @ValidateNested({ each: true })
   @Type(() => CreatePostMetaOptionsDto)
   metaOptions?: CreatePostMetaOptionsDto[];
 }
-
-
