3bc76b57dbe6c1c77c697c516b9cf35dac694d84
NadirBakhsh <nadir4k2010@gmail.com>
2025-03-19 12:10:40 +0500

    Expanding Entity Definition

--- src/Users/dtos/create-user.dtos.ts
+++ src/Users/dtos/create-user.dtos.ts
@@ -5,6 +5,7 @@ import {
   IsOptional,
   IsEmail,
   Matches,
+  MaxLength,
 } from 'class-validator';
 
 export class CreateUserDto {
@@ -21,11 +22,13 @@ export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
   @IsEmail()
+  @MaxLength(100)
   email: string;
-
+  
   @IsString()
   @Length(8, 20)
   @IsNotEmpty()
+  @MaxLength(100)
   @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
     message:
       'Password must be at least 8 characters long, contain one letter, one number, and one special character.',
