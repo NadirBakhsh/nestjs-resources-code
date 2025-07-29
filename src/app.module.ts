1db6c99b132961e67107532883c3f54f29fcb749
NadirBakhsh <nadir4k2010@gmail.com>
2025-07-29 20:18:01 +0500

    nject Database Details

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TagsModule } from './tags/tags.module';
 import { MetaOptionsModule } from './meta-options/meta-options.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 const ENV = process.env.NODE_ENV || 'development';
 @Module({
@@ -19,15 +19,15 @@ const ENV = process.env.NODE_ENV || 'development';
       envFilePath: !ENV ? '.env.development' : `.env.${ENV}`,
     }),
     TypeOrmModule.forRootAsync({
-      imports: [],
-      inject: [],
-      useFactory: () => ({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: 'pg123',
-        database: 'nestjs-blog',
+        port: +configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER') ,
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
         // entities: [__dirname + '/**/*.entity.{ts,js}'],
         autoLoadEntities: true,
         synchronize: true,
