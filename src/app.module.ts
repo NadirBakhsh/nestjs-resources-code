2e7760ce80dfa32b4cad29344e90ef8cc8a59a8f
NadirBakhsh <nadir4k2010@gmail.com>
2025-07-30 17:29:49 +0500

    Custom configuration Files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { TagsModule } from './tags/tags.module';
+import { appConfig } from './config/app.config';
 import { MetaOptionsModule } from './meta-options/meta-options.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-
+import { PostsModule } from './posts/posts.module';
+import { TagsModule } from './tags/tags.module';
+import { UsersModule } from './users/users.module';
 const ENV = process.env.NODE_ENV || 'development';
 @Module({
   imports: [
-    UsersModule, PostsModule, AuthModule,
+    UsersModule,
+    PostsModule,
+    AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
       // envFilePath: ['.env.development', '.env.production'],
       envFilePath: !ENV ? '.env.development' : `.env.${ENV}`,
+      load: [appConfig],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
@@ -24,13 +27,13 @@ const ENV = process.env.NODE_ENV || 'development';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: 'localhost',
-        port: +configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USER') ,
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
         // entities: [__dirname + '/**/*.entity.{ts,js}'],
-        autoLoadEntities: true,
-        synchronize: true,
+        autoLoadEntities: configService.get('database.autoLoadEntities'),
+        synchronize: configService.get('database.synchronize'),
       }),
     }),
     TagsModule,
