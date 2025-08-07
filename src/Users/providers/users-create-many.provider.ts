29adaca8fc6d27ca79d4722dd8bbb52040805617
NadirBakhsh <nadir4k2010@gmail.com>
2025-08-07 19:22:37 +0500

    solution Handle Exceptions for CreateManyUsers

--- src/Users/providers/users-create-many.provider.ts
+++ src/Users/providers/users-create-many.provider.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { CreateManyUsersDto } from '../dtos/create-many-users.dto';
 import { User } from '../user.entity';
@@ -6,16 +6,12 @@ import { User } from '../user.entity';
 @Injectable()
 export class UsersCreateManyProvider {
   constructor(private readonly dataSource: DataSource) {}
+
   public async createMany(createManyUsersDto: CreateManyUsersDto) {
     let newUsers: User[] = [];
-
-    // create query runner instance
     const queryRunner = this.dataSource.createQueryRunner();
 
-    // connect query runner to database
     await queryRunner.connect();
-
-    // start transaction
     await queryRunner.startTransaction();
 
     try {
@@ -24,18 +20,17 @@ export class UsersCreateManyProvider {
         let result = await queryRunner.manager.save(newUser);
         newUsers.push(result);
       }
-
       await queryRunner.commitTransaction();
     } catch (error) {
       await queryRunner.rollbackTransaction();
+      throw new HttpException(
+        'Failed to create users',
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
     } finally {
-      // release query runner
       await queryRunner.release();
     }
 
     return newUsers;
-    // if successFul commit
-    // if un-successFul rollback
-    // Release Connection
   }
 }
