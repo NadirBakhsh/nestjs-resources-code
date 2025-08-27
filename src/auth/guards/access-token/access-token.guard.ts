6e7f722a2c7d1184b3798317be64a186bd72f5a2
NadirBakhsh <nadir4k2010@gmail.com>
2025-08-27 18:09:44 +0500

    149. Completing AccessTokenGuard Implementation

--- src/auth/guards/access-token/access-token.guard.ts
+++ src/auth/guards/access-token/access-token.guard.ts
@@ -1,9 +1,10 @@
-import { CanActivate, ExecutionContext, Inject, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Observable } from 'rxjs';
 import jwtConfig from 'src/auth/config/jwt.config';
 import { Request } from 'express';
+import { REQUEST_USER_KEY } from 'src/auth/constants/auth.constants';
 @Injectable()
 export class AccessTokenGuard implements CanActivate {
   constructor(
@@ -17,6 +18,17 @@ export class AccessTokenGuard implements CanActivate {
     //  extract the token from the request headers
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
+    if(!token) {
+      throw new UnauthorizedException('No token provided');
+    };
+
+    try {
+      const payload = this.jwtService.verifyAsync(token, this.jwtConfiguration);
+      request[REQUEST_USER_KEY] = payload;
+    } catch  {
+      throw new UnauthorizedException('Invalid token');
+    }
+
     return true;
   }
 
