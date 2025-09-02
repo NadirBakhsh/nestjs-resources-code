aa9b7e65b3868203920cb8219914d9eaec3acb75
NadirBakhsh <nadir4k2010@gmail.com>
2025-09-02 18:24:14 +0500

    160. Create Active User Decorator

--- src/auth/providers/sign-in.provider.ts
+++ src/auth/providers/sign-in.provider.ts
@@ -5,6 +5,7 @@ import { SigninDto } from '../dtos/signin.dto';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigType } from '@nestjs/config';
 import jwtConfig from '../config/jwt.config';
+import { ActiveUserData } from '../interfaces/active-user.interface';
 
 @Injectable()
 export class SignInProvider {
@@ -44,7 +45,7 @@ export class SignInProvider {
         const accessToken = this.jwtService.signAsync({
             sub: user.id,
             email: user.email
-        },{
+        } as ActiveUserData ,{
             audience: this.jwtConfiguration.audience,
             issuer: this.jwtConfiguration.issuer,
             secret: this.jwtConfiguration.secret,
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,55 +1,67 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+  Req,
+} from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreatePostDto } from './dtos/createPost.dto';
 import { PatchPostDto } from './dtos/patch-post-dto';
 import { PostsService } from './providers/posts.service';
 import { GetPostsDto } from './dtos/get-posts-dto';
 import { REQUEST_USER_KEY } from 'src/auth/constants/auth.constants';
+import { ActiveUser } from './decorators/active-user.decorator';
+import { ActiveUserData } from 'src/auth/interfaces/active-user.interface';
 @ApiTags('Posts')
 @Controller('posts')
 export class PostsController {
-    // Injecting Posts Service
-    constructor(private readonly postsService: PostsService) {}
+  // Injecting Posts Service
+  constructor(private readonly postsService: PostsService) {}
 
-    @Get('/:userId?')
-    public getPosts(@Param("userId") userId: string, @Query() postQuery: GetPostsDto) {
-        console.log("postQuery:", postQuery);
-        return this.postsService.findAll(postQuery,userId);
-    }
+  @Get('/:userId?')
+  public getPosts(
+    @Param('userId') userId: string,
+    @Query() postQuery: GetPostsDto,
+  ) {
+    console.log('postQuery:', postQuery);
+    return this.postsService.findAll(postQuery, userId);
+  }
 
-    @ApiOperation({
-        summary: 'Create a new post',
-    })
-    @ApiResponse({
-        status: 201,
-        description: 'The record has been successfully created.',
-    })
-    @Post()
-    public createPost(
-        @Req() request,
-        /*
-        @Body() createPostDto: CreatePostDto
-        */
-    ) {
-        console.log("request.user:", request[REQUEST_USER_KEY]);
-        // return this.postsService.create(createPostDto);
-    }
+  @ApiOperation({
+    summary: 'Create a new post',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+  })
+  @Post()
+  public createPost(
+    @Body() createPostDto: CreatePostDto,
+    @ActiveUser() user: ActiveUserData,
+  ) {
+    console.log('Active User:', user);
+  }
 
-    @ApiOperation({
-        summary: 'Update a post',
-    })
-    @ApiResponse({
-        status: 200,
-        description: 'The record has been successfully updated.',
-    })
-    @Patch()
-    public updatePost(@Body() patchPostDto: PatchPostDto) {
-        return this.postsService.update(patchPostDto);
-    }
+  @ApiOperation({
+    summary: 'Update a post',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The record has been successfully updated.',
+  })
+  @Patch()
+  public updatePost(@Body() patchPostDto: PatchPostDto) {
+    return this.postsService.update(patchPostDto);
+  }
 
-    @Delete()
-    public deletePost(@Query('id', ParseIntPipe) id: number) {
-        return this.postsService.delete(id);
-    }
-
+  @Delete()
+  public deletePost(@Query('id', ParseIntPipe) id: number) {
+    return this.postsService.delete(id);
+  }
 }
