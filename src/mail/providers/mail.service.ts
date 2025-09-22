29ed831287aa7279d85606ad831c5344a7aa3b73
NadirBakhsh <nadir4k2010@gmail.com>
2025-09-22 23:05:53 +0500

    196. Creating MailService

--- src/mail/providers/mail.service.ts
+++ src/mail/providers/mail.service.ts
@@ -1,4 +1,26 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+    constructor(
+        private readonly mailerService: MailerService,
+    ) {}
+
+    public async sendUserWelcome(user: User): Promise<void> {
+        await this.mailerService.sendMail({
+            to: user.email,
+            from: 'Onboarding Team <kKlOJ@example.com>',
+            subject: 'Welcome to the App! Confirm your Email',
+            template: './welcome', 
+            context: {
+                name: user.firstName,
+                email: user.email,
+                loginUrl: 'http://localhost:3000',
+            },
+        })
+    }
+
+
+}
